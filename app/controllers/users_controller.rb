e978b81cea835e769dcd225fd283b99214c953e2
Vincent <vincethewalker@yahoo.co.uk>
2020-08-07 18:37:35 +0100

    Completed Sessions Controller set up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,25 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new 
+    @user = User.new
+  end
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "User Created"
+      redirect_to user_path(@user)
+  end
+
   def show 
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params 
+    params.require(:user).permit(:username, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get 'messages/index'
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
 end
