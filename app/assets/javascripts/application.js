80ea89d5ba9b94cb6e446eda13302453ea575318
Vincent <vincethewalker@yahoo.co.uk>
2020-08-10 12:46:46 +0100

    Changes to submit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,6 +23,17 @@ scroll_bottom = function() {
   }
 }
 
+submit_message = function() {
+  $('#message_body').on('keydown', function(e) {
+    if (e.keyCode == 13 ) {
+      $('button').click();
+      e.target.value = " ";
+    };
+  }); 
+};
+
+
+
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
   $('.message .close')
@@ -31,5 +42,6 @@ $(document).on('turbolinks:load', function() {
       .closest('.message').transition('fade');
   });
   scroll_bottom();
+  submit_message();
 })
 
