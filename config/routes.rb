0e0c4f23b020aeee3e24e6c3a337c6a4f8fcf7b2
Vincent <vincethewalker@yahoo.co.uk>
2020-08-10 17:16:54 +0100

    Added User sign up and css changeds to login and index pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
  root 'chatroom#index'
+
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'login', to: 'sessions#destroy'
   post 'message', to: 'messages#create'
-
+  resources :users
   mount ActionCable.server, at: '/cable'
 end
