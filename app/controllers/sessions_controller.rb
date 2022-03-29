603035e6391ac6e653a01049bc9e50d3b743bd01
Vincent Walker <vincethewalker@yahoo.co.uk>
2022-03-29 13:29:08 +0200

    Resolving web-sockets access error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   def create 
     user = User.find_by(username: params[:session][:username].downcase)
       if user && user.authenticate(params[:session][:password])
-        session[:user_id] = user.id
+        session[:user_id] = current_user.id
         flash[:success] = "You are Logged in" 
         redirect_to chatroom_path
       else
